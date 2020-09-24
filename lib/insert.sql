33a355b9f6308847ee08bdbf684e862eb007fe50
dunow <dunow33@hotmail.com>
2020-09-23 20:55:26 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 32, "male", "brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Rowdy", 3, "female", "white", "mad", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Tabitha", 39, "male", "black", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Seargeant Brown", 21, "female", "orange", "mad", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Melissa", 13, "male", "red", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Grinch", 45, "female", "black", "happy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Wendy", 98, "male", "yello", "sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) values (null, 32, "female", "grey", "calm", 0);
